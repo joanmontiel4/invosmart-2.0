772bd885a00578487deda5ceb74a9f0ad0f87caa
joanmontiel4 <joanmontiel@gmail.com>
2022-10-03 13:18:24 +0200

    Add a tool to print labels of object keys

--- src/infrastructures/tools/labels.ts
+++ src/infrastructures/tools/labels.ts
@@ -3,4 +3,11 @@ export const labels: { [key: string]: string } = {
     email: 'Email',
     phoneNumber: 'Phone Number',
     address: 'Address',
+    invoiceNumber: 'Invoice Number',
+    invoiceDate: 'Invoice Date',
+    invoiceDueDate: 'Invoice Due Date',
+    subtotal: 'Subtotal',
+    tax: 'Tax (21%)',
+    withholdingIRPF: 'Withholdings on account of Income Tax (IRPF)',
+    total: 'Total',
 };
